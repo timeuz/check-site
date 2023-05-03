cceb17cc89d6d20505852278688abc1662f8c29f
Timeu <timeu.fo@gmail.com>
2023-05-03 13:51:01 -0300

    fixing api url

--- app.py
+++ app.py
@@ -37,35 +37,38 @@ def check_website():
         message = str(e)
     return render_template('home.html', message=message, latency=latency, total_time=total_time, status=status, url=url)
 
-@app.route('/api/check_website', methods=['GET', 'POST'])
-def api_check_website():
-    total_time = None
-    latency = None
-    status = None
-    url = request.json['url']
+
+
+@app.route('/api/<path:url>', methods=['GET', 'POST'])
+def api_check_website(url):
+    total_time = 0
+    latency = 0
+    status = 0
     if not url.startswith('http'):
         url = 'http://' + url
+
+#    response = None
+
     try:
         start_time = time.time()
         response = requests.get(url, timeout=5)
         latency = round((time.time() - start_time), 2)
         total_time = round(response.elapsed.total_seconds(), 2)
         if response.status_code == 200:
-            #message = f"{url} is reachable with {latency} s latency. Total loading time: {total_time} s"
-            message = f"{url} is reachable"
-            status = "success"
+            message = f"{url} esta vivo"
+            status = f"sucesso {str(response.status_code)}"
         else:
-            message = f"{url} returned a {response.status_code} error"
-            status = "error"
+            message = f"{url} retornou um erro {response.status_code}"
+            status = f"erro {str(response.status_code)}"
     except requests.exceptions.Timeout:
-        message = f"{url} timed out"
-        status = "error"
+        message = f"{url} time out"
+        status = f"erro 408"
     except requests.exceptions.ConnectionError:
-        message = f"{url} could not be reached"
-        status = "error"
+        message = f"{url} nao pode ser alcancado"
+        status = f"erro 500"
     except Exception as e:
         message = str(e)
-        status = "error"
+        status = f"erro 500"
 
     return jsonify({'status': status, 'message': message, 'latency': latency, 'total_time': total_time})
 
